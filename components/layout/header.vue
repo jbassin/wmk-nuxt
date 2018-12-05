9493fedf1d9a6b33922ec0580abafb3bf216aa2e
Joshua Bassin <jbassin@bu.edu>
2018-12-05 16:29:09 -0500

    added header

---
+++ components/layout/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container has-text-centered">
+    <div class="notification is-primary">
+      <p class="title is-1">
+        Waterdeep: Keys of the Mad Mage
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutHeader',
+};
+</script>
+
+
+<style scoped>
+  .container {
+    margin-top: 10px;
+  }
+</style>
