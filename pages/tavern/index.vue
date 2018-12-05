47f3e992e0687287495703e7d0176115d797632b
Joshua Bassin <jbassin@bu.edu>
2018-12-05 17:08:47 -0500

    initialized tavern component

---
+++ pages/tavern/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container animated fadeIn">
+    <div class="notification is-primary">
+      <p class="title is-4">
+        {{ tavernName }}: Status Report
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tavernName: 'The Floating Cup',
+    };
+  },
+  head() {
+    return {
+      title: 'WKMM: Tavern',
+    };
+  },
+};
+</script>
+
+<style>
+  .container {
+    margin-top: 10px;
+  }
+</style>
