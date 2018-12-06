8089bb5a33db10d78ddbfe2793d3eff83871c8fd
Joshua Bassin <jbassin@bu.edu>
2018-12-06 12:44:22 -0500

    added 404

---
+++ components/layout/error.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container animated fadeIn">
+    <div class="notification is-primary">
+      <p
+        class="title is-4">
+        You seem to be lost... Error Code {{ error.statusCode }}
+      </p>
+      <span>
+        This isn't a valid url on this webpage. You should probably try going somewhere that isn't here. That would help.
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutError',
+  props: {
+    error: {
+      type: Object,
+      default() {
+        return {
+          statusCode: 404,
+        };
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,36 @@
+<template>
+  <layout-error
+    :error="error"
+    class="container-margin"/>
+</template>
+
+<script>
+import LayoutHeader from '~/components/layout/header';
+import LayoutNavbar from '~/components/layout/navbar';
+import LayoutError from '../components/layout/error';
+
+export default {
+  components: {
+    LayoutError,
+    LayoutHeader,
+    LayoutNavbar,
+  },
+  props: {
+    error: {
+      type: Object,
+      default() {
+        return {
+          statusCode: 404,
+        };
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .container-margin {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+</style>
