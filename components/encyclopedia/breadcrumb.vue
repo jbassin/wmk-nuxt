abb0c3bcba2ea390542832512dfbaf94affdb4b1
Joshua Bassin <jbassin@bu.edu>
2018-12-11 18:44:29 -0500

    added breadcrumbs

--- components/encyclopedia/breadcrumb.vue
+++ components/encyclopedia/breadcrumb.vue
@@ -1,17 +1,59 @@
 <template>
   <nav class="breadcrumb is-right">
-    <ul>
-      <li><a href="#">Bulma</a></li>
-      <li><a href="#">Documentation</a></li>
-      <li><a href="#">Components</a></li>
-      <li class="is-active"><a href="#">Breadcrumb</a></li>
+    <ul v-if="links.length > 3">
+      <li>
+        <nuxt-link :to="links[0].path">
+          {{ links[0].name }}
+        </nuxt-link>
+      </li>
+      <li>
+        <a>
+          ...
+        </a>
+      </li>
+      <li
+        v-for="(item, index) in require('ramda').takeLast(2, links)"
+        :key="index">
+        <nuxt-link :to="item.path">
+          {{ item.name }}
+        </nuxt-link>
+      </li>
+    </ul>
+    <ul v-else>
+      <li
+        v-for="(item, index) in links"
+        :key="index">
+        <nuxt-link :to="item.path">
+          {{ item.name }}
+        </nuxt-link>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script>
+const R = require('ramda');
+
 export default {
   name: 'EncyclopediaBreadcrumb',
+  props: {
+    breadcrumbPath: {
+      required: true,
+      type: Array,
+    },
+  },
+  computed: {
+    links() {
+      return R.reduce((o, n) => R.concat(o, [{
+        name: n
+          .toLowerCase()
+          .split('_')
+          .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(' '),
+        path: `${R.last(o) === undefined ? '/encyclopedia' : R.last(o).path}/${n}`,
+      }]), [], this.breadcrumbPath);
+    },
+  },
 };
 </script>
 
--- pages/encyclopedia/_.vue
+++ pages/encyclopedia/_.vue
@@ -11,11 +11,11 @@
         </div>
         <div class="level-right">
           <div class="level-item">
-            <encylopedia-breadcrumb/>
+            <encylopedia-breadcrumb :breadcrumb-path="require('ramda').tail(breadcrumbPath.split('/'))"/>
           </div>
         </div>
       </nav>
-      {{ $nuxt.$route.path }}
+      {{ path }}
     </div>
   </div>
 </template>
@@ -23,9 +23,26 @@
 <script>
 import EncylopediaBreadcrumb from '~/components/encyclopedia/breadcrumb.vue';
 
+const R = require('ramda');
+
 export default {
   components: {
     EncylopediaBreadcrumb,
+  },
+  computed: {
+    path() {
+      return R.tail(R.tail(this.$nuxt.$route.path.split('/')));
+    },
+  },
+  async asyncData({ app, params }) {
+    const { path } = await app.$axios.$get('/api/entries/path', {
+      params: {
+        root: R.last(params.pathMatch.split('/')),
+      },
+    });
+    return {
+      breadcrumbPath: path === '/' ? null : path,
+    };
   },
 };
 </script>
