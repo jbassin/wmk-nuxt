22065ecb5e6729212ee4570cc092bfcd59639897
Joshua Bassin <jbassin@bu.edu>
2018-12-11 16:29:00 -0500

    added encyclopedia index

---
+++ pages/encyclopedia/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="notification is-primary">
+      <nav class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <p class="title is-4">
+              Encyclopedia
+            </p>
+          </div>
+        </div>
+        <div class="level-right">
+          <div class="level-item">
+            AHHHHHHH
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+
+</style>
